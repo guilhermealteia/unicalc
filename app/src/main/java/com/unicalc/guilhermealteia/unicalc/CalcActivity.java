cec5c9535d0efa146635255425475c4fee684842
Guilherme Alteia Alves Ponciano <guilherme.ponciano@bradesco.com.br>
2019-01-15 16:41:52 -0200

    Ajustes na nova tela de cálculo

--- app/src/main/java/com/unicalc/guilhermealteia/unicalc/CalcActivity.java
+++ app/src/main/java/com/unicalc/guilhermealteia/unicalc/CalcActivity.java
@@ -3,11 +3,16 @@ package com.unicalc.guilhermealteia.unicalc;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.RelativeLayout;
 
 public class CalcActivity extends AppCompatActivity {
 
@@ -18,7 +23,28 @@ public class CalcActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Oculta a barra de título do app
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_calc);
+
+        ConstraintLayout constraintLayout = findViewById(R.id.constraintLayout);
+
+        RelativeLayout relativeLayout = findViewById(R.id.relativeLayout);
+
+        AlphaAnimation anim = new AlphaAnimation(0.0f, 1.0f);
+        anim.setDuration(1500);
+        anim.setFillAfter(true);
+        anim.setFillBefore(true);
+        constraintLayout.startAnimation(anim);
+
+//
+//        anim = new AlphaAnimation(0.0f, 1.0f);
+//        anim.setDuration(3000);
+//        anim.setFillBefore(true);
+//        relativeLayout.startAnimation(anim);
+
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 //
