aaca8dce9f3a60eb3d6eb449b1b53fc7d7a8d1b9
Guilherme Alteia <alteia@live.com>
2019-01-30 22:42:14 -0200

    Criando nova activity

---
+++ app/src/main/java/com/unicalc/guilhermealteia/unicalc/MainActivity.java
@@ -0,0 +1,18 @@
+package com.unicalc.guilhermealteia.unicalc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+        //Oculta a barra de título do app
+        getSupportActionBar().hide();
+    }
+}
